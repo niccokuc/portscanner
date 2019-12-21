6d19345b33a157c2d200dee4989cb3b8fa73304f
root <nerminkuc74@gmail.com>
2019-12-21 23:06:23 +1100

    .

--- AdvancePortscanner6.1.py
+++ AdvancePortscanner6.1.py
@@ -3,17 +3,22 @@
 from socket import *
 import optparse
 from threading import *
+opnprt = 0
+clsprt = 0
 
 def connScan(tgtHost, tgtPort):
+    opnprt=0
+    clsprt=0
     try:
         sock = socket(AF_INET, SOCK_STREAM)
         sock.connect((tgtHost, tgtPort))
         print('[+] %d/tcp Open') % tgtPort
+        opnprt = opnprt + 1
     except:
-        print('[-] %d/tcp Closed') % tgtPort
+        #print('[-] %d/tcp Closed') % tgtPort
+        clsprt+=1
     finally:
         sock.close()
-
 
 def portScan(tgtHost, tgtPorts, tgtRange):
     # resolve the domain name into an IP. use get host by name.
@@ -29,7 +34,7 @@ def portScan(tgtHost, tgtPorts, tgtRange):
     except:
         print('[+] Scan results for: ' + tgtIP)
 
-    setdefaulttimeout(1)
+    setdefaulttimeout(5)
 
     if tgtPorts != ['None']:
         print("[+] Scanning specific ports now:")
@@ -44,11 +49,10 @@ def portScan(tgtHost, tgtPorts, tgtRange):
             t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
             t.start()
 
-
 def main():
-    parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target ports>')
+    parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target ports, a,b,c,d...> -r <target port range, n,m>')
     parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-    parser.add_option('-p', dest='tgtPort', type='string', help='specify target ports sperated by comma')
+    parser.add_option('-p', dest='tgtPort', type='string', help='specify target ports separated by comma')
     parser.add_option('-r', dest='tgtRange', type='string', help='specify target Range, eg. 25,100')
     (options, args) = parser.parse_args()
     tgtHost = options.tgtHost
@@ -59,7 +63,6 @@ def main():
         print(parser.defaults)
         exit(0)
     portScan(tgtHost, tgtPorts, tgtRange)
-
 
 if __name__ == '__main__':
     main()
